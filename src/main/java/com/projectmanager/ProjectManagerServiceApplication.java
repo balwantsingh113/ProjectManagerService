afa1be3ea145cfc4037aa37c932d7dbc1a14f7e2
Administrator <Administrator@172.18.1.141>
2020-04-22 10:59:42 +0530

    Updated

--- src/main/java/com/projectmanager/ProjectManagerServiceApplication.java
+++ src/main/java/com/projectmanager/ProjectManagerServiceApplication.java
@@ -1,5 +1,5 @@
 package com.projectmanager;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
